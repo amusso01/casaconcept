5d493bda326dcc7a47756cf636949cf91530ba1d
amusso01 <eandram@hotmail.it>
2022-03-02 14:48:25 +0100

    404

--- 404.php
+++ 404.php
@@ -10,17 +10,23 @@
 get_header(); ?>
 
 
-<main class="main main-error" role="main">
+<main class="main main-error content-block" role="main">
 
-	<section class="section" id="section-error-page">
+	<section class="section" id="section-error-page" style="margin:120px 0 ;">
 
 		<h1 class="page-title error-title"><?php esc_html_e( 'Content Cannot Be Found', 'foundry' ); ?></h1>
 
-		<div class="page-body error-body">
+		<div class="page-body error-body" style="margin:80px 0 120px;">
 
-			<p><?php esc_html_e( 'Unfortunately the content you were looking for could not be found. Please check that the URL is correct or do a search using the form below.', 'foundry' ); ?></p>
+			<p style="margin:30px 0 ;"><?php esc_html_e( 'Unfortunately the content you were looking for could not be found. Please check that the URL is correct or do a search using the form below.', 'foundry' ); ?></p>
 
-			<?php get_search_form(); ?>
+      <form id="searchForm404" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+          <input type="hidden" name="post_type[]" value="product" />
+          <input type="text" class="search-field" name="s" placeholder="SEARCH" value="<?php echo get_search_query(); ?>">
+          <button type="submit" id="searchSubmit">
+            <?php get_template_part( 'svg-template/svg', 'icon_search' ) ?>
+          </button>
+      </form>
 
 		</div>
 
